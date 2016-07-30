e67ba108ee565f1ff7d8008f2360c9c4fa5c44f3
Alex Glover <alexdglover@gmail.com>
2016-07-30 16:47:32 -0500

    removes debugging alert msg

--- app.js
+++ app.js
@@ -361,7 +361,6 @@ $(document).ready(function() {
           console.log(data);
           gritterWrapper('Updated org', "Your org name was updated successfully!", 'green-check-200px.png');
           user.userName = $('#updaterUserFormUserName').val();
-          alert(user);
           $('#profile-nickname').text(user.userName);
           // populateUserPanel();
         }).fail(function(jqXHR, textStatus, errorThrown){
